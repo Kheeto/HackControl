2387eba055530fea9174f69aa707d398fbc6c5ff
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-03-01 20:59:58 +0100

    Update Message.java

--- java/kheeto/hackcontrol/Message.java
+++ java/kheeto/hackcontrol/Message.java
@@ -2,6 +2,7 @@ package kheeto.hackcontrol;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Message {
     public static void send(CommandSender sender, String message) {
@@ -10,5 +11,13 @@ public class Message {
 
     public static void send(CommandSender sender, String message, String prefix) {
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + message));
+    }
+
+    public static void send(Player target, String message) {
+        send(target, message, HackControl.getInstance().getConfig().getString("prefix"));
+    }
+
+    public static void send(Player target, String message, String prefix) {
+        target.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + message));
     }
 }
