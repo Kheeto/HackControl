4f6a2f48a84fdd1320384cee4b9bb6ff27f66cb4
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-03-01 20:59:41 +0100

    Update and rename ControlCommand.java to Control.java

--- java/kheeto/hackcontrol/commands/Control.java
+++ java/kheeto/hackcontrol/commands/Control.java
@@ -6,6 +6,7 @@ import kheeto.hackcontrol.Message;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 import java.util.Dictionary;
@@ -22,33 +23,39 @@ public class Control {
         new CommandBase("control", false) {
             @Override
             public boolean onCommand(CommandSender sender, String[] args) {
+                FileConfiguration config = plugin.getConfig();
+
                 if (args.length == 0) {
-                    Message.send(sender, plugin.getConfig().getStringList("help.control").toString());
+                    Message.send(sender, config.getStringList("help.control").toString());
                     return true;
                 }
 
                 // Starts a new hack control
                 if (args[0] == "start") {
                     Player target = Bukkit.getPlayer(args[1]);
+                    if (target == null) Message.send(sender, config.getString("error.noPlayer"));
 
                     // Executed from console
                     if (!(sender instanceof Player)) {
-                        Message.send(sender, plugin.getConfig().getString("errors.notPlayer"));
+                        Message.send(sender, config.getString("errors.notPlayer"));
                         return false;
                     }
                     // Executed by a player
                     if (controlList.get(target.getUniqueId()) == Bukkit.getPlayer(sender.getName()).getUniqueId()) {
                         if (sender.hasPermission("hackcontrol.control.start")) {
                             controlList.put(target.getUniqueId(), ((Player) sender).getUniqueId());
+                            Message.send(sender, config.getString("control.stafferControlMessage"));
+                            Message.send(target, config.getString("control.playerControlMessage"));
                             return true;
                         }
-                        else Message.send(sender, plugin.getConfig().getString("errors.noPermission"));
+                        else Message.send(sender, config.getString("errors.noPermission"));
                     }
                 }
 
                 // Stops an hack control that is currently ongoing
                 if (args[0] == "cancel") {
                     Player target = Bukkit.getPlayer(args[1]);
+                    if (target == null) Message.send(sender, config.getString("error.noPlayer"));
 
                     // Executed from console
                     if (!(sender instanceof Player)) {
@@ -57,24 +64,28 @@ public class Control {
                     }
                     // Executed by the same staffer who is controlling the player
                     if (controlList.get(target.getUniqueId()) == Bukkit.getPlayer(sender.getName()).getUniqueId()) {
-                        if (sender.hasPermission("hackcontrol.control.cancel")) {
-                            controlList.remove(target.getUniqueId());
-                            return true;
+                        if (!sender.hasPermission("hackcontrol.control.cancel")) {
+                            Message.send(sender, config.getString("errors.noPermission"));
+                            return false;
                         }
-                        else Message.send(sender, plugin.getConfig().getString("errors.noPermission"));
                     }
                     // Executed by another staffer
                     else {
-                        if (sender.hasPermission("hackcontrol.control.cancel.others")) {
-                            controlList.remove(target.getUniqueId());
-                            return true;
+                        if (!sender.hasPermission("hackcontrol.control.cancel.others")) {
+                            Message.send(sender, config.getString("errors.noPermission"));
+                            return false;
                         }
-                        else Message.send(sender, plugin.getConfig().getString("errors.noPermission"));
                     }
+
+                    // Removes the target from the list of players in hack control
+                    controlList.remove(target.getUniqueId());
+                    Message.send(sender, config.getString("control.stafferControlMessage"));
+                    Message.send(target, config.getString("control.playerControlMessage"));
+                    return true;
                 }
 
                 if (args[0] == "setup") {
-
+                    // TODO position setup commands
                 }
 
                 return false;
