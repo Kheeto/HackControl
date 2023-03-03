0046865ba07e7bc71b1df373f64a5c7d2cd3dc85
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-03-03 17:20:20 +0100

    Update HackControl.java

--- java/kheeto/hackcontrol/HackControl.java
+++ java/kheeto/hackcontrol/HackControl.java
@@ -16,6 +16,10 @@ public final class HackControl extends JavaPlugin {
         saveDefaultConfig();
 
         new Control(this).LoadLocations();
+
+        Freeze freeze = new Freeze();
+        getCommand("freeze").setExecutor(freeze);;
+        getCommand("unfreeze").setExecutor(freeze);;
     }
 
     @Override
