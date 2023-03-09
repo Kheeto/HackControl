8cd62cb6932fa3375a9bab52e9a35d3c565dc13d
Kheeto <70845116+Kheeto@users.noreply.github.com>
2023-03-09 19:01:16 +0100

    Update HackControl.java

--- java/kheeto/hackcontrol/HackControl.java
+++ java/kheeto/hackcontrol/HackControl.java
@@ -2,11 +2,15 @@ package kheeto.hackcontrol;
 
 import kheeto.hackcontrol.commands.Control;
 import kheeto.hackcontrol.commands.Freeze;
+import kheeto.hackcontrol.gui.GUIConfig;
+import kheeto.hackcontrol.gui.GUIListener;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.awt.*;
 
 public final class HackControl extends JavaPlugin {
 
+    @Getter
     private static HackControl instance;
 
     @Override
@@ -14,22 +18,26 @@ public final class HackControl extends JavaPlugin {
         instance = this;
 
         saveDefaultConfig();
+        GUIConfig.setup(this);
+        GUIConfig.getConfig().options().copyDefaults(true);
+        GUIConfig.save();
 
         Control control = new Control(this);
         control.LoadLocations();
         getCommand("control").setExecutor(control);
         getCommand("control").setTabCompleter(control);
+        getServer().getPluginManager().registerEvents(control, this);
 
         Freeze freeze = new Freeze(this);
         getCommand("freeze").setExecutor(freeze);
         getCommand("freeze").setTabCompleter(freeze);
         getServer().getPluginManager().registerEvents(freeze, this);
+
+        getServer().getPluginManager().registerEvents(new GUIListener(), this);
     }
 
     @Override
     public void onDisable() {
         getLogger().severe(Color.RED + "HackControl plugin was disabled!");
     }
-
-    public static HackControl getInstance() { return instance; }
 }
